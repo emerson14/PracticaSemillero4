337ff31026a20e90fc7993544dbb2426e666062a
emerson14 <49541510+emerson14@users.noreply.github.com>
2022-06-13 12:34:01 -0500

    Add files via upload
    Juego fecha de nacimiento 
    index
    css
    main

---
+++ main.js
@@ -0,0 +1,17 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!'
+    }});
+    
+    var name = "nombre";
+    var number = "age"
+
+    function Adivinar(){
+      const PreguntasYRespuestas =[];
+      Preguntas.forEach(element => {
+
+      });
+      }
+
+      var Personas =["JoseA",["MariaP","LocoC","EmerG","NataR","LuceroL"]]
